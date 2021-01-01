539dc9d8ba2068cd77ed94180e288fa7218c3a90
jingwenMC <2313055824@qq.com>
2021-01-02 00:04:42 +0800

    Release Ready

--- CommandBridge-spigot/src/main/java/cn/southplex/commandbridge/commands/SendCmd.java
+++ CommandBridge-spigot/src/main/java/cn/southplex/commandbridge/commands/SendCmd.java
@@ -1,20 +1,25 @@
 package cn.southplex.commandbridge.commands;
 
 import cn.southplex.commandbridge.CommandBridgeSpigot;
+import cn.southplex.commandbridge.LogUtil;
 import com.google.common.collect.Iterables;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.logging.Level;
+
 public class SendCmd implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(sender instanceof Player)return false;
+        if(sender instanceof Player)return true;
+        if(args==null)sender.sendMessage(ChatColor.RED+"Error: Null Command");
         ByteArrayDataOutput out = ByteStreams.newDataOutput();
         out.writeUTF("commandbridge");
         String outs = null;
@@ -24,7 +29,8 @@ public class SendCmd implements CommandExecutor {
             else outs = outs+" "+a;
         out.writeUTF(outs);
         Player player = Iterables.getFirst(Bukkit.getOnlinePlayers(), null);
-        player.sendPluginMessage(CommandBridgeSpigot.getInstance(),"BungeeCord",out.toByteArray());
+        if(player==null) LogUtil.log(Level.WARNING,"No player is online to send a plugin message");
+        else player.sendPluginMessage(CommandBridgeSpigot.getInstance(),"BungeeCord",out.toByteArray());
         return true;
     }
 }
