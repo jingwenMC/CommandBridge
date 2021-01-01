59d184e649d03f0bd47a529e60f67747713127d9
jingwenMC <2313055824@qq.com>
2021-01-01 23:25:13 +0800

    Various Upd

--- CommandBridge-bungeecord/src/main/java/cn/southplex/commandbridge/CommandBridgeBungee.java
+++ CommandBridge-bungeecord/src/main/java/cn/southplex/commandbridge/CommandBridgeBungee.java
@@ -1,12 +1,14 @@
 package cn.southplex.commandbridge;
 
+import cn.southplex.commandbridge.enums.ServerType;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public final class CommandBridgeBungee extends Plugin {
 
     @Override
     public void onEnable() {
-
+        new LogUtil(getProxy().getLogger());
+        new ServerStatus(ServerType.BUNGEE);
         // Plugin startup logic
     }
 
--- CommandBridge-spigot/src/main/java/cn/southplex/commandbridge/CommandBridgeSpigot.java
+++ CommandBridge-spigot/src/main/java/cn/southplex/commandbridge/CommandBridgeSpigot.java
@@ -1,11 +1,15 @@
 package cn.southplex.commandbridge;
 
+import cn.southplex.commandbridge.enums.ServerType;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CommandBridgeSpigot extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        new LogUtil(Bukkit.getLogger());
+        new ServerStatus(ServerType.SPIGOT);
         // Plugin startup logic
 
     }
